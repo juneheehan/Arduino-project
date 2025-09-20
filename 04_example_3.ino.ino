2853aff701450102bddf1a0dac4e37c55b4a0693
한준희 <junehee.han06@gmail.com>
2025-09-20 14:55:28 +0900

    Add files via upload

---
+++ 04_example_3.ino.ino
@@ -0,0 +1,26 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200); //Initialize serial port
+  while (!Serial) {
+  delay(2000) 
+    
+    ; // wait for serial port to connect
+  }
+  Serial.println("Hello World!");
+  count = toggle = 0;
+  digitalWrite(PIN_LED, toggle);
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = !toggle;
+  digitalWrite(PIN_LED, toggle);
+  delay(1000);
+}
+
+int toggle_state(int toggle) {
+  return toggle;
+}
